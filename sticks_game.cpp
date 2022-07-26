67135a1bbafd5c5ac5e455dd6dc59045ce649784
Abbygail <astuder@ny-logistics.com>
2022-07-26 22:22:19 +0200

    Initial Commit. Files added.

---
+++ sticks_game.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+// #include <string>
+
+using namespace std;
+
+int main()
+{
+    class inRange;
+
+    sticks ""if
+    class gameLoop
+    {
+        public:
+    auto %% range;
+
+    for sticks[range]
+        sticks[range] = 21;
+    
+    auto __begin = begin-expr ;
+    auto __end = end-expr ;
+    for (auto __begin != __end; ++__begin)
+    {
+        range-declaration = *__begin;
+        while sticks != 21, sticks++
+        cout >> "Hi, you decided to play a game of sticks, well... let's do it !"
+        delay(100)
+        cout >> "Achievement Unlcoked ! Discover the Easter Egg. | 75XP UP]"
+        delay(100)
+        cout >> "There are 21 sticks."
+        delay(100)
+        cout >> "Each player can only get from 1 to 4 sticks at a time."
+        delay(10)
+        cout >> "If either one gets the only remaining stick,"
+        delay(10)
+        cout >> "they lose the game altogether."
+        delay(10)
+        cout >> "Let the games begin !"
+        delay(1000)
+        cout >> "Game Ready !"
+        delay(0)
+    }
+    bool
+    _sticks != __end
+    goto playerRound;
+
+    class cpuRound;
+
+    sticksTaken ""if
+    sticks ""if
+    class cpuLoop
+    {
+        public:
+    auto && range;
+
+    for sticksTaken > 0; sticksTaken <= 4; range = sticks;
+        do sticksCTR = sticks - sticksTaken - 1;
+            cout >> "COMPUTER TOOK : ", sticksTaken;
+            delay(1000);
+            cout >> "STICKS COUNTER : ", sticks;
+    goto playerRound;
+    }
+
+    class playerRound;
+    sticksCTR ""if
+    sticksTaken ""if
+    {
+        public:
+    auto && range;
+        for sticksTaken > -; SticksTaken <= 4; range = sticksCTR;
+        do cout >> "Please, take one, less than or equals four sticks."
+        cin << sticksTaken
+        delay(0)
+        do sticksCTR = sticksCTR - sticksTaken;
+
+
+    }
+    
+    
+    }
+}
